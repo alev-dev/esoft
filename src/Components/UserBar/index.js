f5624d17208fdaed36c3aaede842c1d7054e5ab2
Alejandro Alfonso Vargas <alejandro.alfonso@onlive.site>
2022-05-25 17:26:04 -0300

    update

--- src/Components/UserBar/index.js
+++ src/Components/UserBar/index.js
@@ -2,16 +2,24 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
 import Logo from '../../assets/logo.png';
 import { setUser } from '../../redux/slicers/userSlice';
+import { FaBars } from 'react-icons/fa';
+import { useState } from 'react';
 
 function UserBar() {
     const user = useSelector((state) => state.user);
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const [optionBar, setoptionBar] = useState(false);
 
     const handleLogout = () => {
+        setoptionBar(false);
         localStorage.removeItem('user');
         dispatch(setUser(null));
         navigate('/');
+    };
+
+    const handleOptionBar = () => {
+        setoptionBar(!optionBar);
     };
 
     return (
@@ -23,7 +31,7 @@ function UserBar() {
                     </div>
                 </Link>
                 <div className="esoft-navbar-menu">
-                    <ul>
+                    <ul className="esoft-navbar-menu-links">
                         <li className="esoft-navbar-menu-item">
                             <Link to="/home">Home</Link>
                         </li>
@@ -43,6 +51,36 @@ function UserBar() {
                             </li>
                         )}
                     </ul>
+                    <button className="esoft-navbar-menu-showOptions" onClick={handleOptionBar}>
+                        <FaBars />
+                    </button>
+                    {optionBar && (
+                        <ul
+                            className="esoft-navbar-menu-links-mobile"
+                            onClick={() => {
+                                setoptionBar(false);
+                            }}
+                        >
+                            <li className="esoft-navbar-menu-item">
+                                <Link to="/home">Home</Link>
+                            </li>
+                            <li className="esoft-navbar-menu-item">
+                                <Link to="/about">Sobre</Link>
+                            </li>
+                            <li className="esoft-navbar-menu-item">
+                                <Link to="/content">Conteudo</Link>
+                            </li>
+                            {user?.userlogged ? (
+                                <li className="esoft-navbar-menu-item" onClick={handleLogout}>
+                                    Logout
+                                </li>
+                            ) : (
+                                <li className="esoft-navbar-menu-item">
+                                    <Link to="/authentication/login">Login</Link>
+                                </li>
+                            )}
+                        </ul>
+                    )}
                 </div>
             </div>
         </nav>
