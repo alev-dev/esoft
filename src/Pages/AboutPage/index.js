605c3dd88d79a923a93be67a9c1535c19a84baa7
Alejandro Alfonso Vargas <alejandro.alfonso@onlive.site>
2022-05-27 20:36:40 -0300

    asd

--- src/Pages/AboutPage/index.js
+++ src/Pages/AboutPage/index.js
@@ -1,5 +1,16 @@
+import './style.css';
+import Photo from '../../assets/naan.jfif';
 function AboutPage() {
-    return <h1>About Page</h1>;
+    return (
+        <div className="esoft-content">
+            <h1>About</h1>
+            <img className="about-photo" src={Photo} alt="" />
+            <h3>
+                Sistema desenvolvido baseado na matéria Topicos Avançados de Software. Sistema tem como objetivo
+                integrar aluno e professor para facilitar a comunicação e forma de ensino.
+            </h3>
+        </div>
+    );
 }
 
 export default AboutPage;
