e4ab00127b4ca71f939cad40838b79264b73ae31
Alejandro Alfonso Vargas <alejandro.alfonso@onlive.site>
2022-05-30 12:12:40 -0300

    asd

--- src/App.js
+++ src/App.js
@@ -3,21 +3,13 @@ import './App.css';
 import AppRouter from './routes/AppRouter';
 import { Provider, useDispatch } from 'react-redux';
 import store from './store';
-import io from 'socket.io-client';
 import { useEffect, useState } from 'react';
 import { setUser } from './redux/slicers/userSlice';
 import { getUser, isAuthenticated } from './common/services/token';
 import NotificationContainer from 'react-notifications/lib/NotificationContainer';
 
 function App() {
-    const [socket, setSocket] = useState(null);
     const dispatch = useDispatch();
-
-    useEffect(() => {
-        const newSocket = io(`http://localhost:3000`);
-        setSocket(newSocket);
-        return () => newSocket.close();
-    }, [setSocket]);
 
     useEffect(() => {
         if (isAuthenticated()) {
